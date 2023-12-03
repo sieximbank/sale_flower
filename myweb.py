cddcc9571e146795e90452d205b4a6386269e8ce
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:19:00 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Trang chủ", 'Giới thiệu'], 
+    selected = option_menu(["Trang chủ", 'Giới thiệu'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     #selected
         
