eb1826a40ddbdc55a14dbf82220f59182e54d5d6
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:37:49 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -4,9 +4,9 @@ st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BET SHOP")
 with st.container():
     col1,col2 = st.columns(2)
     with col1:
-        st.header("HOA NHẬP KHẨU")
+        st.write("HOA NHẬP KHẨU")
     with col2:
-        st.header("HOA TRONG NƯỚC")
+        st.write("HOA TRONG NƯỚC")
 options = st.sidebar.multiselect(
     'Bạn thích Hoa nào nè :',
     ['Hồng', 'Cúc', 'Hướng dương', 'Mâm xôi'])
