52d3b5f374867e314bdd01a8242b65d820f0fdce
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:21:15 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],  
     menu_icon="cast", default_index=0, orientation="horizontal")
     selected2
