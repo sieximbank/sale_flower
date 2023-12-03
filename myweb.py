22a2ec686c2dbbcb08cf8034ab7ab0ca66a17693
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:36:49 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
 st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BET SHOP")
+with st.container():
+    col1,col2 = st.columns(2)
+    with col1:
+        st.header("HOA NHẬP KHẨU")
+    with col2:
+        st.header("HOA TRONG NƯỚC")
 options = st.sidebar.multiselect(
     'Bạn thích Hoa nào nè :',
     ['Hồng', 'Cúc', 'Hướng dương', 'Mâm xôi'])
