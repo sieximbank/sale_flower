b4dfa4b73867d115cf1643331689aee6f50a6110
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:12:45 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.write("HƯƠNG - HUYỀN FLOWERS SHOP")
+        # app = st.sidebar(
+        with st.sidebar:
+            app = option_menu(
+                menu_title='demo',
+                options=['Trang Chủ','About'],
+                icons=['house-fill','person-circle','trophy-fill','chat-fill','$
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'$
+        "icon": {"color": "white", "font-size": "23px"},
