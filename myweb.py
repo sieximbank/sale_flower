c8d601130c2d32774a5786e12dc74d89cf19522e
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-03 15:17:28 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.write("HƯƠNG - HUYỀN FLOWERS SHOP")
-        # app = st.sidebar(
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Trang chủ", 'Giới thiệu'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+        
+#st.write("HƯƠNG - HUYỀN FLOWERS SHOP")
+#        # app = st.sidebar(
