a1023d71b92c33f368f6350e13d90ad294c7c19c
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-12 20:52:42 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 
+page_bg_image = """
+<style>
+[data-testid="stAppViewContainer"]{
+background_image: url("https://unsplash.com/photos/I2UR7wEftf4"
+}
+</style>
+"""
+st.markdown(page_bg_image, unsafe_allow_html=True)
+st.title("Flower Shop")
