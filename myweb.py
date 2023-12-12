c8291186112b6cce079276c3b8f27f79a3942cb7
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-12 20:43:31 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
 
-#st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BET SHOP")
-st.image('sunrise.jpg')
+
+st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BET SHOP")
+
