9fffc15f7b57e65f0665e9c1fcb7089c296cd0b4
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-12 21:26:19 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
-
-st.image("https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg", width = 800)
+bg_image="https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg"
+st.image(bg_image, caption= "Hoa Tuoi", width = 800, channels = "RGB")
