3874cff77100630fda2f6651d953e59d4e9ad3df
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-12 21:14:02 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 
-st.image("https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg", caption="Hoa tuoi"))
+st.image("https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg", caption="Hoa tuoi")
