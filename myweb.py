c42c5519247380aeddbb4a8a2274ae835a952da2
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-14 20:39:43 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -1,5 +1,15 @@
 import streamlit as st
 #from streamlit_option_menu import option_menu
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 
 st.header("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 
