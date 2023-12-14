d3010474fd9ca835ea032ba4867e51490fe60c20
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-14 20:33:34 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -17,4 +17,4 @@ st.header("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 
 tab1, tab2, tab3 = st.tabs(["Home", "Admin", "About"])
 with tab1:
-   st.siderbar.multiselect("Chọn hoa :",['Hoa Hồng','Hoa Lan','Hoa Nhập Khẩu'])
+   st.sidebar.multiselect("Chọn hoa :",['Hoa Hồng','Hoa Lan','Hoa Nhập Khẩu'])
