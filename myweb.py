b7be3776e571c4bb8926238735d1b56f7079c776
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-14 20:45:49 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -30,5 +30,3 @@ st.header("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 
 st.sidebar.multiselect("Chọn hoa",['Hoa Hồng','Hoa Lan','Hoa Nhập Khẩu'])
 st.sidebar.multiselect("Chọn Loại",['Nhập khẩu','Trong nước'])
-
-t.markdown( """ <style> .sidebar .sidebar-content { background-image: linear-gradient(#2e7bcf,#2e7bcf); color: blue; } </style> """, unsafe_allow_html=True, ) 
