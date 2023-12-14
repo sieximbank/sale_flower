95be48c9d73559f61574b8ffad180007b285ae65
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-14 19:33:27 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -4,12 +4,16 @@ st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 #bg_image="https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg"
 #st.image(bg_image, caption= "Hoa Tuoi", width = 800, channels = "RGB")
 #st.title("Hoa Tuoi")
-with st.sidebar:
-	selected = option_menu("Main Menu", ["Home", "Admin"], 
-	    icons=['house','gear'], 
-	    menu_icon="cast", default_index=0,
-	    styles={
-	        "container": {"padding": "0!important", "background-color": "#fafafa"},
-	        "icon": {"color": "blue", "font-size": "14px"}, 
-	        "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-	        "nav-link-selected": {"background-color": "green"},})
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
