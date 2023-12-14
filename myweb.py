40530050fa20aa641c34b62514c28e581ed96423
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-14 19:34:17 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -4,7 +4,7 @@ st.write("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 #bg_image="https://images.unsplash.com/photo-1695653421371-cd48246a6200?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=grab-ftRu8P06Z5Y-unsplash.jpg"
 #st.image(bg_image, caption= "Hoa Tuoi", width = 800, channels = "RGB")
 #st.title("Hoa Tuoi")
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2, tab3 = st.tabs(["Trang chủ", "Admin", "About"])
 
 with tab1:
    st.header("A cat")
