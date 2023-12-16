31aafeee1ef4707e62402879ee5c648328c29643
sieximbank <70384312+sieximbank@users.noreply.github.com>
2023-12-16 11:03:07 +0700

    Update myweb.py

--- myweb.py
+++ myweb.py
@@ -32,7 +32,9 @@ st.header("HUYỀN - HƯƠNG FLOWERS SHOP - THE BEST SHOP")
 st.sidebar.multiselect("Chọn hoa",['Hoa Hồng','Hoa Lan','Hoa Nhập Khẩu'])
 #st.sidebar.multiselect("Chọn Loại",['Nhập khẩu','Trong nước'])
 #https://drive.google.com/file/d/1ePE3SQzf1SVip928h70QoYm6JXFqhmHi/view?usp=sharing
-col1,col2,col3 = st.columns(3,gap='small')
+#col1,col2,col3 = st.columns(3,gap='small')
+with st.container(border=True):
+   st.write("Hình nền")        
 #with col1:
 #        image = Image.open('https://drive.google.com/file/d/1ePE3SQzf1SVip928h70QoYm6JXFqhmHi/view?usp=sharing','r')
 #        st.image(image)
